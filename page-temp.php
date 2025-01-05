91bc8201c40e70321f5afbe0896d36a25c874d86
throne <contact@throne.work>
2025-01-05 15:39:35 -0800

    removing images

--- page-temp.php
+++ page-temp.php
@@ -1,3 +1,27 @@
 <?php /* Template name: Temp */ ?>
 
-temp
+<style>
+    body {
+        background: #111;
+        color: #fff;
+    }
+
+    .temp-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    svg {
+        width: 40px;
+    }
+</style>
+
+<?php get_header(); ?>
+
+<div class="temp-container">
+    <?php get_template_part('/template-parts/icon--throne'); ?>
+</div>
+
+<?php get_footer(); ?>
